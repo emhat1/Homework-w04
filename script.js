9380cc8a33b6fb3cf9ed95473e56fa12c7fd7241
emhat1 <93974950+emhat1@users.noreply.github.com>
2021-12-12 19:20:07 +1100

    Beginning the project - initial uploads

---
+++ script.js
@@ -0,0 +1,42 @@
+
+
+//Array of questions for the quiz
+const questions = [
+    {
+        questionText: "In JavaScript, what brackets surround a function?",
+        answerOptions: ["1. Normal brackets ()", "2. Square brackets []", "3. Curly brackets {}", "4. Triangular brackets <>"],
+        correctAnswer: "3",
+    }
+    {
+        questionText: "What kind of data can be stored in JavaScript arrays?",
+        answerOptions: ["1. Numbers", "2. Strings", "3. Booleans", "4. All of the above"],
+        correctAnswer: "4",
+    }
+    {
+        questionText: "What is the primary function of JavaScript in web pages?",
+        answerOptions: ["1. To store basic content", "2. To alter the appearance of the page", "3. To provide functionality to the page and allow interaction", "4. All of the above"],
+        correctAnswer: "3",
+    }
+    {
+        questionText: "Which of the below is a JavaScript command?",
+        answerOptions: ["1. class='MainHeading'", "2. font-family=serif", "3. meta charset='UTF-8'", "4. var name = document.getElementById('name')"],
+        correctAnswer: "4"
+    }
+    {
+        questionText: "What is the correct way to call the random method on the Math global object?",
+        answerOptions: ["1. random.Math()", "2. Math(random)", "3. Math.random()", "4. math.random()"],
+        correctAnswer: "3",
+    }
+    {
+        questionText: "What is string concatenation?",
+        answerOptions: ["1. When you print string to the console", "2. When you join strings together", "3. When you assign a string to a variable", "4. When you change a variable's value"],
+        correctAnswer: "2",
+        
+    }
+]
+
+
+//Timer
+
+
+//High scores
