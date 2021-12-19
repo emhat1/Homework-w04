772af9793aff0b226b5d307063335e3b44144a1b
Erin H <e.hatherell@gmail.com>
2021-12-19 18:57:02 +1100

    Leaving out quotation marks makes things messy - fixed now

--- script.js
+++ script.js
@@ -12,33 +12,33 @@ const questions = [
         questionText: "In JavaScript, what brackets surround a function?",
         answerOptions: ["1. Normal brackets ()", "2. Square brackets []", "3. Curly brackets {}", "4. Triangular brackets <>"],
         correctAnswer: "3. Curly brackets {}",
-    }
+    },
     {
         questionText: "What kind of data can be stored in JavaScript arrays?",
         answerOptions: ["1. Numbers", "2. Strings", "3. Booleans", "4. All of the above"],
         correctAnswer: "4. All of the above",
-    }
+    },
     {
         questionText: "What is the primary function of JavaScript in web pages?",
         answerOptions: ["1. To store basic content", "2. To alter the appearance of the page", "3. To provide functionality to the page and allow interaction", "4. All of the above"],
         correctAnswer: "3. To provide functionality to the page and allow interaction",
-    }
+    },
     {
         questionText: "Which of the below is a JavaScript command?",
         answerOptions: ["1. class='MainHeading'", "2. font-family=serif", "3. meta charset='UTF-8'", "4. var name = document.getElementById('name')"],
-        correctAnswer: "4. var name = document.getElementById('name')""
-    }
+        correctAnswer: "4. var name = document.getElementById('name')"
+    },
     {
         questionText: "What is the correct way to call the random method on the Math global object?",
         answerOptions: ["1. random.Math()", "2. Math(random)", "3. Math.random()", "4. math.random()"],
         correctAnswer: "3. Math.random()",
-    }
+    },
     {
         questionText: "What is string concatenation?",
         answerOptions: ["1. When you print string to the console", "2. When you join strings together", "3. When you assign a string to a variable", "4. When you change a variable's value"],
         correctAnswer: "2. When you join strings together",
         
-    }
+    },
 ]
 
 //Organise cards and label with variables
@@ -71,7 +71,7 @@ function setTime() {
 }
 
 //Let's get things started!
-document.querySelector("#start-btnn").addEventListener("click", startQuiz);
+document.querySelector("#start-btn").addEventListener("click", startQuiz);
 
 function startQuiz() {
     hideCards();
@@ -181,10 +181,19 @@ function clearHighscores() {
   renderLeaderboard();
 }
 
+//Display the leaderboard
+var leaderboardBtn = document.querySelector(".leaderboard");
+leaderboardBtn.addEventListener("click", leaderboardDisplay);
+
+var leaderboardDisplay = function() {
+    leaderboardCard.setAttribute("hidden", false);
+}
+
+
+//Return to starting page
 const backBtn = document.querySelector("#back-btn");
 backBtn.addEventListener("click", returnToStart);
 
-//Return to starting page
 function returnToStart() {
   hideCards();
   startCard.removeAttribute("hidden");
